a621182f75553c614d218dec9f830303bd342cb1
ezra.mcavoy <miximaxa13427@gmail.com>
2019-11-20 11:03:35 +0000

    favorite

--- resources/views/microposts/microposts.blade.php
+++ resources/views/microposts/microposts.blade.php
@@ -9,7 +9,16 @@
                 <div>
                     <p class="mb-0">{!! nl2br(e($micropost->content)) !!}</p>
                 </div>
-                <div>
+                <div class="d-flex flex-row">
+                    @if (Auth::user()->is_favorite($micropost->id))
+                        {!! Form::open(["route" => ["favorites.unfavorite", $micropost->id], "method" => "delete"]) !!}
+                           {!! Form::submit("Unfavorite", ["class" => "btn btn-info btn-sm mr-1"]) !!}
+                        {!! Form::close() !!}
+                    @else
+                        {!! Form::open(["route" => ["favorites.favorite", $micropost->id]]) !!}
+                            {!! Form::submit("Favorite", ["class" => "btn btn-success btn-sm mr-1"]) !!}
+                        {!! Form::close() !!}
+                    @endif
                     @if (Auth::id() == $micropost->user_id)
                         {!! Form::open(['route' => ['microposts.destroy', $micropost->id], 'method' => 'delete']) !!}
                             {!! Form::submit('Delete', ['class' => 'btn btn-danger btn-sm']) !!}
